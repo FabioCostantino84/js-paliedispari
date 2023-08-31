3bc53886ad408d3c6c1479839205f1c5bdc72ab3
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-31 17:45:55 +0200

    ex palin aad traccia

--- palidroma/main.js
+++ palidroma/main.js
@@ -1,1 +1,11 @@
-console.log('ciao');
+/* 
+
+Palidroma
+Chiedere all’utente di inserire una parola 
+Creare una funzione per capire se la parola inserita è palindroma
+
+*/
+
+//chiedere una parola all'utente
+
+//invertire la parola per capire se è palindroma
