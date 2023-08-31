3b252229403d28a899ad3d71ca59f19df069e37c
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-31 15:08:34 +0200

    ex pari e dispari add pseudo codice

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log('hello world');
+
+/* 
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+*/
+
+//utente sceglie pari o dispari
+
+//utente sceglie un numero da 1 a 5
+
+//generare un numero random da 1 a 5 per il computer
+
+//sommare il numero scelto da utente a quello random del computer
+
+//stabilire se è pari o dispari
+
+//dichiarare chi ha vinto
